c0d541a373a1f1a05289e1cf53218540876ec8f4
AbisurDiazR <abisurdiazramirez@gmail.com>
2020-07-09 13:42:04 -0500

    "Final Commit"

--- app/src/main/java/com/example/pokedex/MainActivity.java
+++ app/src/main/java/com/example/pokedex/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.pokedex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.telephony.CellIdentity;
@@ -22,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
     //creamos la instancia global de retrofit
     private Retrofit retrofit;
 
+    private RecyclerView recyclerView;
+    private ListaPokemonAdapter listaPokemonAdapter;
+
+    private int offset;
+
+    private boolean aptoParaCargar;
+
     //variable local para el log
     private String TAG = "Pokedex";
 
@@ -30,6 +40,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        listaPokemonAdapter = new ListaPokemonAdapter(this);
+        recyclerView.setAdapter(listaPokemonAdapter);
+        recyclerView.setHasFixedSize(true);
+        final GridLayoutManager layoutManager = new GridLayoutManager(this,3);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener(){
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (dy > 0){
+                    int visibleItemCount = layoutManager.getChildCount();
+                    int totalItemCount = layoutManager.getItemCount();
+                    int pastVisibleItems = layoutManager.findFirstVisibleItemPosition();
+
+                    if (aptoParaCargar){
+                        if ((visibleItemCount + pastVisibleItems) >= totalItemCount){
+                            Log.i(TAG," Llegamos al final.");
+
+                            aptoParaCargar = false;
+                            offset += 20;
+                            obtenerDatos(offset);
+                        }
+                    }
+                }
+            }
+        });
+
         //inicializamos la url base
         retrofit = new Retrofit.Builder()
                 .baseUrl("http://pokeapi.co/api/v2/")
@@ -37,10 +76,12 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         //obtenerDatos() es una interface de java, retrofit trabaja bien con ellas
-        obtenerDatos();
+        aptoParaCargar = true;
+        offset = 0;
+        obtenerDatos(offset);
     }
 
-    private void obtenerDatos(){
+    private void obtenerDatos(int offset){
         PokeApiService service = retrofit.create(PokeApiService.class);
         Call<PokemonRespuesta> pokemonRespuestaCall = service.obtenerListaPokemon();
 
@@ -51,10 +92,11 @@ public class MainActivity extends AppCompatActivity {
                     PokemonRespuesta pokemonRespuesta = response.body();
                     //hacemos un arraylist para guardar los pokemon de pokemonRespuesta
                     ArrayList<Pokemon> listaPokemon = pokemonRespuesta.getResults();
+                    listaPokemonAdapter.adicionarListaPokemon(listaPokemon);
 
                     for(int i=0; i < listaPokemon.size(); i++){
                         Pokemon p = listaPokemon.get(i);
-                        Log.i(TAG," Pokemon: "+p.getName());
+                        Log.i(TAG," Pokemon: "+p.getName()+" Number: "+p.getNumber());
                     }
 
                 }else{
